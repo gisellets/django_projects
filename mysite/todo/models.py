40db92eeff6212563fd1907586bb8fcdbba3c4f1
Giselle <gisellevi002@gmail.com>
2025-02-20 12:55:34 -0500

    working on todo list

--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Item(model.Model):
+class Item(models.Model):
     item_text = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.question_text
+        return self.item_text
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Item
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    model = Item
+
+
+class DetailView(generic.DetailView):
+    model = Item
