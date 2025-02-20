a71f565902aefdd3c19f1b89c34c56ac638ebbcb
Giselle <gisellevi002@gmail.com>
2025-02-19 21:41:41 -0500

    made it to step 5

--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-model = item 
+class Item(model.Model):
+    item_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.question_text
