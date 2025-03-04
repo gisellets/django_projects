a4a57d4b75d1a11d90dc125f0f36607d227ce935
Giselle <gisellevi002@gmail.com>
2025-03-04 12:54:27 -0500

    cats crud app works

--- mysite/cats/models.py
+++ mysite/cats/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Cat(models.Model):
-    cat_text = models.CharField(max_length=200)
+    cat_name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.cat_text
+        return self.cat_name
 
     def get_absolute_url(self):
         return "/cats/%i/" % self.id
